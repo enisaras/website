c81127b12b7175aa9603fb33881525686b9dc5c2
Enis <arass@rpi.edu>
2021-03-06 21:46:52 -0500

    commit

--- src/components/Widecard.js
+++ src/components/Widecard.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import Social from '../components/Social'
+import Social from '../components/Social';
+import Collapsible from 'react-collapsible';
 class Widecard extends Component {
 render() {
 return (
 <div class="widecard">
 <div class="compdet">
-<h3>{this.props.title}</h3>
-<h4 class= "secondtext">{this.props.where}</h4>
-<h4 class= "secondtext">{this.props.gpa}</h4>
-<h4 class= "secondtext">{this.props.from} - {this.props.to}</h4>
+<Collapsible trigger = {this.props.title}>
+  <h3 class= "secondtext">{this.props.where}</h3>
+  <h3 class= "secondtext">{this.props.gpa}</h3>
+  <h3 class= "secondtext">{this.props.from} - {this.props.to}</h3>
+  <Collapsible trigger = {this.props.coursework}>
+    <h3 class = "secondtext">{this.props.course1}</h3>
+    <h3 class = "secondtext">{this.props.course2}</h3>
+  </Collapsible>
+
+  </Collapsible>
 <Social />
 </div>
 </div>
