3975aa10883c56039d02cf5eced228a260287245
arass <arass@rpi.edu>
2021-03-07 01:44:35 -0500

    added collapsible
    Signed-off-by: arass <arass@rpi.edu>

--- src/components/Widecard.js
+++ src/components/Widecard.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import Social from '../components/Social'
+import Social from '../components/Social';
+import Collapsible from 'react-collapsible';
 class Widecard extends Component {
 render() {
 return (
-<div class="widecard">
 <div class="compdet">
-<h3>{this.props.title}</h3>
+<Collapsible trigger = {this.props.title} open = "true">
 <h4 class= "secondtext">{this.props.where}</h4>
 <h4 class= "secondtext">{this.props.gpa}</h4>
 <h4 class= "secondtext">{this.props.from} - {this.props.to}</h4>
+<Collapsible trigger = {this.props.coursework} open = "false">
+    <Collapsible classParentString = "b" trigger = {this.props.course1}>
+
+    </Collapsible>
+</Collapsible>
+</Collapsible>
 <Social />
-</div>
 </div>
 )
 }
--- src/contents/Education.js
+++ src/contents/Education.js
@@ -4,9 +4,9 @@ class Education extends Component {
 render() {
 return (
 <div className="condiv">
-<h1 className="subtopic">My Education</h1>
+<h2 className="subtopic">Click on collapsibles to view coursework!</h2>
 <Widecard title="Rensselaer Polytechnic Instute" where="Computer Science" gpa="GPA: 3.3"
-from="Fall 2020" to="Spring 2022"/>
+from="Fall 2020" to="Spring 2022" coursework = "Relevant Coursework"/>
 <Widecard title="Hudson Valley Community College" where="Computer Science" gpa="GPA: 4.0"
 from="Summer 2019" to="Summer 2020"/>
 </div>
