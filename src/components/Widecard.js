7f5a577a833a3810b295f6ab1c3115c0a651a360
Enis <arass@rpi.edu>
2021-03-07 00:01:08 -0500

    Revert "commit"
    This reverts commit c81127b12b7175aa9603fb33881525686b9dc5c2.

--- src/components/Widecard.js
+++ src/components/Widecard.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import Social from '../components/Social';
-import Collapsible from 'react-collapsible';
+import Social from '../components/Social'
 class Widecard extends Component {
 render() {
 return (
 <div class="widecard">
 <div class="compdet">
-<Collapsible trigger = {this.props.title}>
-  <h3 class= "secondtext">{this.props.where}</h3>
-  <h3 class= "secondtext">{this.props.gpa}</h3>
-  <h3 class= "secondtext">{this.props.from} - {this.props.to}</h3>
-  <Collapsible trigger = {this.props.coursework}>
-    <h3 class = "secondtext">{this.props.course1}</h3>
-    <h3 class = "secondtext">{this.props.course2}</h3>
-  </Collapsible>
-
-  </Collapsible>
+<h3>{this.props.title}</h3>
+<h4 class= "secondtext">{this.props.where}</h4>
+<h4 class= "secondtext">{this.props.gpa}</h4>
+<h4 class= "secondtext">{this.props.from} - {this.props.to}</h4>
 <Social />
 </div>
 </div>
--- src/contents/Education.js
+++ src/contents/Education.js
@@ -5,12 +5,10 @@ render() {
 return (
 <div className="condiv">
 <h1 className="subtopic">My Education</h1>
-  <Widecard title="Rensselaer Polytechnic Instute" where="Computer Science" gpa="GPA: 3.3"
-  from="Fall 2020" to="Spring 2022" coursework = "Relevant Coursework"
-  course1 = "Computer Organization" course2 = "Introduction to Algorithms"/>
-  <Widecard title="Hudson Valley Community College" where="Computer Science" gpa="GPA: 4.0"
-  from="Summer 2019" to="Summer 2020" coursework = "Relevant Coursework"
-  course1 = "Introduction to Computer Science" course2 = "Data Structures"/>
+<Widecard title="Rensselaer Polytechnic Instute" where="Computer Science" gpa="GPA: 3.3"
+from="Fall 2020" to="Spring 2022"/>
+<Widecard title="Hudson Valley Community College" where="Computer Science" gpa="GPA: 4.0"
+from="Summer 2019" to="Summer 2020"/>
 </div>
 )
 }
