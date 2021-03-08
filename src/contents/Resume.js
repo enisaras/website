eafa245bb0e43b6fa2a4bb7dae363dd09633f84a
arass <arass@rpi.edu>
2021-03-08 12:14:47 -0500

    added the pdf loader
    Signed-off-by: arass <arass@rpi.edu>

--- src/contents/Resume.js
+++ src/contents/Resume.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-import Social from '../components/Social';
-class Contact extends Component {
-render() {
-return (
-<div className="condiv">
-<h1 className="subtopic">Resume coming soon</h1>
-<Social />
-</div>
-)
+import React from "react";
+import SinglePagePDFViewer from "../components/pdf-viewer/single-page";
+
+
+/* This is required only if the project file is located 
+inside the app. Otherwise you can use the external link of the pdf file*/
+import samplePDF from "../img/sample.pdf";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h4>Resume is coming soon</h4>
+        {//<SinglePagePDFViewer pdf={samplePDF} />
+        }
+
+      <hr />
+    </div>
+  );
 }
-}
-export default Contact
